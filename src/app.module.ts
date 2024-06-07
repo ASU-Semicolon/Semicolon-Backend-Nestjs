86d1556d6f5624206e3091b9eb5ba5414f6331f0
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-07 22:55:55 +0300

    feat: add swagger and mongoose modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { WorkshopsModule } from './workshops/workshops.module';
 import { CommitteesModule } from './committees/committees.module';
 import { ApplicantsModule } from './applicants/applicants.module';
 import { ParticipantsModule } from './participants/participants.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule, WorkshopsModule, CommitteesModule, ApplicantsModule, ParticipantsModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UsersModule,
+        WorkshopsModule,
+        CommitteesModule,
+        ApplicantsModule,
+        ParticipantsModule,
+        MongooseModule.forRoot(process.env.DB_URL),
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(new ValidationPipe({}));
+
+    app.setGlobalPrefix('api');
+
+    const config = new DocumentBuilder()
+        .setTitle("Semicolon's API")
+        .setDescription(
+            "API to manage Semicolon's members, workshops, committees, and more",
+        )
+        .setVersion('1.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+
+    await app.listen(process.env.PORT);
 }
 bootstrap();
