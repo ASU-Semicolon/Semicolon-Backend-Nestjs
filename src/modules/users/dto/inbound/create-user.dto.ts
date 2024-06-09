1a6a9bf8ab14edcaff665f94c7db0913be91f618
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-09 18:17:43 +0300

    refactor: users DTOs

--- src/modules/users/dto/inbound/create-user.dto.ts
+++ src/modules/users/dto/inbound/create-user.dto.ts
@@ -14,6 +14,7 @@ import mongoose from 'mongoose';
 export class CreateUserDto {
     @ApiProperty({
         description: 'Email of the user',
+        example: 'test@email.com',
         type: String,
         required: true,
     })
@@ -22,17 +23,19 @@ export class CreateUserDto {
 
     @ApiProperty({
         description: 'Username of the user',
+        example: 'testuser',
         type: String,
         required: true,
     })
     @IsString()
-    @MinLength(4)
+    @MinLength(3)
     username: string;
 
     @ApiProperty({
         description: 'Password of the user',
         type: String,
         required: true,
+        example: 'password123',
     })
     @MinLength(8)
     password: string;
@@ -41,6 +44,7 @@ export class CreateUserDto {
         description: 'Phone number of the user',
         type: String,
         required: true,
+        example: '01234567890',
     })
     @IsPhoneNumber('EG')
     phone: string;
@@ -49,6 +53,7 @@ export class CreateUserDto {
         description: 'Id of the committee that the user belongs to.',
         type: String,
         required: true,
+        example: '60f7b3b3b5f7f0001f000001',
     })
     @IsMongoId()
     committee: mongoose.Types.ObjectId;
@@ -58,6 +63,7 @@ export class CreateUserDto {
         type: Boolean,
         required: false,
         default: false,
+        example: true,
     })
     @IsBoolean()
     @IsOptional()
@@ -67,6 +73,7 @@ export class CreateUserDto {
         description: 'Role of the user',
         enum: ['admin', 'hr', 'member'],
         required: true,
+        example: 'admin',
     })
     @IsEnum(['admin', 'hr', 'member'])
     role: 'admin' | 'hr' | 'member';
---
+++ src/modules/users/dto/outbound/user.dto.ts
@@ -0,0 +1,96 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, plainToInstance, Transform } from 'class-transformer';
+import mongoose from 'mongoose';
+import { CommitteeDto } from 'src/modules/committees/dto/committee.dto';
+
+export class UserDto {
+    @ApiProperty({
+        description: 'Email of the user',
+        example: 'test@email.com',
+        type: String,
+    })
+    @Expose({
+        name: 'email',
+    })
+    Email: string;
+
+    @ApiProperty({
+        description: 'Username of the user',
+        example: 'testuser',
+        type: String,
+    })
+    @Expose({
+        name: 'username',
+    })
+    Username: string;
+
+    @ApiProperty({
+        description: 'Password of the user',
+        type: String,
+        example: 'password123',
+    })
+    @Expose({
+        name: 'password',
+    })
+    Password: string;
+
+    @ApiProperty({
+        description: 'Phone number of the user',
+        type: String,
+        example: '01234567890',
+    })
+    @Expose({
+        name: 'phone',
+    })
+    Phone: string;
+
+    @ApiProperty({
+        description: 'Id of the committee that the user belongs to.',
+        type: CommitteeDto,
+    })
+    @Expose({
+        name: 'committee',
+    })
+    @Transform(({ value }) =>
+        plainToInstance(CommitteeDto, value, { excludeExtraneousValues: true }),
+    )
+    Committee: CommitteeDto;
+
+    @ApiProperty({
+        description: "Status of user's account",
+        type: Boolean,
+        example: true,
+    })
+    @Expose({
+        name: 'active',
+    })
+    Active: boolean;
+
+    @ApiProperty({
+        description: 'Role of the user',
+        enum: ['admin', 'hr', 'member'],
+        example: 'admin',
+    })
+    @Expose({
+        name: 'role',
+    })
+    Role: 'admin' | 'hr' | 'member';
+
+    @ApiProperty({
+        description: 'Date of user creation',
+        type: Date,
+    })
+    @Expose({
+        name: 'createdAt',
+    })
+    CreatedAt: Date;
+
+    @ApiProperty({
+        description: 'Date of user last update',
+        type: String,
+    })
+    @Expose({
+        name: 'updatedAt',
+    })
+    UpdatedAt: String;
+}
