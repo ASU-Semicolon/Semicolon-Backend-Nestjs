b7715361e20b8ed1303d3a8b15803d6039c6e5e2
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-11 13:10:04 +0300

    refactor: move global pipes and interceptors inside app module

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { TransformInterceptor } from './interceptors/transform.interceptor';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: ['debug', 'warn'],
     });
 
-    app.useGlobalPipes(new ValidationPipe({}));
-
     app.setGlobalPrefix('api');
-
-    app.useGlobalInterceptors(new TransformInterceptor());
 
     const config = new DocumentBuilder()
         .setTitle("Semicolon's API")
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { WorkshopsModule } from './workshops/workshops.module';
 import { CommitteesModule } from './committees/committees.module';
 import { CandidatesModule } from './candidates/candidates.module';
-import { APP_FILTER } from '@nestjs/core';
+import { APP_FILTER, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { MongoExceptionFilter } from 'src/exception-filters/mongoose.filter';
+import { TransformInterceptor } from 'src/interceptors/transform.interceptor';
 
 @Module({
     imports: [
@@ -20,6 +21,14 @@ import { MongoExceptionFilter } from 'src/exception-filters/mongoose.filter';
         {
             provide: APP_FILTER,
             useClass: MongoExceptionFilter,
+        },
+        {
+            provide: APP_PIPE,
+            useClass: ValidationPipe,
+        },
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: TransformInterceptor,
         },
     ],
 })
