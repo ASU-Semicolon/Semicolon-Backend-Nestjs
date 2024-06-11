be0835037b8ba95c89c5bb39565bad7947e1eceb
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-11 12:50:19 +0300

    feat: update season dto
    separate the common decorator into a dedicated decorator
    to be used in many places within the code

--- src/dto/season.dto.ts
+++ src/dto/season.dto.ts
@@ -1,17 +1,31 @@
+import { applyDecorators } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsNumberString, Length } from 'class-validator';
 
 export class SeasonDto {
-    @ApiProperty({
-        description:
-            'Season to filter the entity based on. If not passed all entities without filtering are returned',
-        example: '2024',
-        required: false,
-    })
-    @IsNumberString()
-    @Length(4, 4, {
-        message: 'season must be 4 characters long. For example: 2024',
-    })
+    @SeasonDecorator()
     @IsOptional()
     season: string;
+}
+
+/**
+ * Decorator to contain common validators for season field rather than repeating them in many places.
+ * The decorator doesn't contain IsOptional() decorator, so you need to add it manually if needed.
+ * @param inQuery This is used to change the description of the field based on the location where season resides @default true
+ */
+export function SeasonDecorator(inQuery = true) {
+    let description = inQuery
+        ? 'Season to filter the entity based on. If not passed all entities without filtering are returned'
+        : 'Season where the entity belongs to';
+    return applyDecorators(
+        ApiProperty({
+            description,
+            example: '2024',
+            required: !inQuery,
+        }),
+        IsNumberString(),
+        Length(4, 4, {
+            message: 'season must be 4 characters long. For example: 2024',
+        }),
+    );
 }
