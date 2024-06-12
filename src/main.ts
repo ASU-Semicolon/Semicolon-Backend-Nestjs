f54a6bf6141447168d70c61b000a6301ed35ae01
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-12 13:22:27 +0300

    feat: add bearer auth to swagger

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
             "API to manage Semicolon's members, workshops, committees, and more",
         )
         .setVersion('1.0')
+        .addBearerAuth()
         .build();
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api-docs', app, document);
