8c44ee22dd4c3cdc79d853dc9683ed8fe664d3fb
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-06-12 15:38:39 +0300

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         logger: ['debug', 'warn'],
+        cors: true,
     });
 
     app.setGlobalPrefix('api');
